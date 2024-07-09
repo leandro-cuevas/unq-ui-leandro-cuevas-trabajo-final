ca3d39eb05856f737f560a8e64a78293b585a8c5
Leandro <leandroagustin.cuevas@gmail.com>
2024-07-09 00:08:08 -0300

    cacheo de errores y page not found

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import SelectDifficulty from './SelectDifficulty';
 import Game from './Game';
+import NoExist from './NoExist';
 
 const RouterApp = () => (
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/play" element={<SelectDifficulty />} />
         <Route path="/play/:difficulty" element={<Game />} />
+        <Route path="*" element={<NoExist />} />
     </Routes>
 );
 
